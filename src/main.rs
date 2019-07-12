5bd7d551451283a031b03f5df8697ed19de9bd03
Kevin Per <kevin.per@protonmail.com>
2019-07-12 10:23:32 +0200

    temp

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@ extern crate diesel;
 extern crate serde_derive;
 
 use actix_identity::{CookieIdentityPolicy, IdentityService};
-use actix_web::{middleware, web, App, HttpServer};
+use actix_web::{middleware, web, App, HttpServer, Responder};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
+mod errors;
 mod models;
 mod schema;
-mod errors;
 //mod invitation_handler;
 //mod register_handler;
-mod user_handler;
 mod exists_handler;
+mod user_handler;
 //mod contacts_handler;
 mod utils;
 
@@ -25,17 +25,19 @@ fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL expected");
 
+    /*
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     builder
         .set_private_key_file("key.pem", SslFiletype::PEM)
         .unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
+    */
 
     let manager = ConnectionManager::<PgConnection>::new(database_url);
-    let pool : models::Pool = r2d2::Pool::builder()
+    let pool: models::Pool = r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create a pool");
-    let domain : String = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".into());
+    let domain: String = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".into());
 
     HttpServer::new(move || {
         App::new()
@@ -43,32 +45,32 @@ fn main() {
             .wrap(middleware::Logger::default())
             //.data(web::JsonConfig::default().limit(4096))
             .data(web::JsonConfig::default())
+            .service(web::scope("/test").service(web::resource("/").route(web::get().to(index))))
             .service(
                 web::scope("/api")
-                /*
-                 * .service(
-                    web::resource("/")
-                        .route(web::get().to_async(|| actix_web::HttpResponse::Ok().finish()))
-                )
-                */
-
-                .service(
-                    web::resource("/user/{base_tel}/cc/{country_code}/led/{led}")
-                        .route(web::put().to_async(user_handler::update))
-                )
-                .service(
-                    web::resource("/user/{base_tel}/cc/{country_code}/")
-                        .route(web::get().to_async(user_handler::get))
-                        .route(web::post().to_async(user_handler::add))
-                        .route(web::put().to_async(user_handler::update))
-                )
-                .service(
-                    web::resource("/exists/{base_tel}/cc/{country_code}/")
-                        .route(web::post().to_async(exists_handler::get))
-                )
+                    .service(
+                        web::resource("/user/{base_tel}/cc/{country_code}/led/{led}")
+                            .route(web::put().to_async(user_handler::update)),
+                    )
+                    .service(
+                        web::resource("/user/{base_tel}/cc/{country_code}/")
+                            .route(web::get().to_async(user_handler::get))
+                            .route(web::post().to_async(user_handler::add))
+                            .route(web::put().to_async(user_handler::update)),
+                    )
+                    .service(
+                        web::resource("/exists/{base_tel}/cc/{country_code}/")
+                            .route(web::post().to_async(exists_handler::get)),
+                    ),
             )
-        })
-        .bind_ssl("0.0.0.0:3000", builder).unwrap()
-        .run().unwrap()
+    })
+    //.bind_ssl("0.0.0.0:3000", builder)
+    .bind("0.0.0.0:3000")
+    .unwrap()
+    .run()
+    .unwrap()
+}
 
+fn index() -> impl Responder {
+    format!("Hello")
 }
