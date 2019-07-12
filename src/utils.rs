cc8c3c720a7866472064214f61e9022f3336445b
Kevin Per <kevin.per@protonmail.com>
2019-07-12 09:01:00 +0200

    drop arg

--- src/utils.rs
+++ src/utils.rs
@@ -1,36 +1,7 @@
 use crate::errors::ServiceError;
-use argonautica::{Hasher, Verifier};
 use phonenumber::Mode;
-
-lazy_static::lazy_static! {
-    pub static ref SECRET_KEY: String = std::env::var("SECRET_KEY").unwrap_or_else(|_| "0123".repeat(8));
-}
 
 pub fn phonenumber_to_international(number: &str, country: &str) -> String {
     let number = phonenumber::parse(Some(country.parse().unwrap()), number).unwrap();
     format!("{}", number.format().mode(Mode::International)).replace(" ", "")
-}
-
-pub fn hash_password(password: &str) -> Result<String, ServiceError> {
-    Hasher::default()
-        .with_password(password)
-        .with_secret_key(SECRET_KEY.as_str())
-        .hash()
-        .map_err(|err| {
-            dbg!(err);
-
-            ServiceError::InternalServerError
-        })
-}
-
-pub fn verify(hash: &str, password: &str) -> Result<bool, ServiceError> {
-    Verifier::default()
-        .with_hash(hash)
-        .with_password(password)
-        .with_secret_key(SECRET_KEY.as_str())
-        .verify()
-        .map_err(|err| {
-            dbg!(err);
-            ServiceError::Unauthorized
-        })
 }
