9d21c0729d473e4aeb75fc673561ae302cf09682
Kevin Per <kevin.per@protonmail.com>
2019-12-27 21:47:49 +0100

    init migration tool for hashing telephone numbers

---
+++ migration_software/sha256_hasher/README.md
@@ -0,0 +1,4 @@
+# SHA256-Hasher
+
+This is migration tool because I switched in Gehma from plaintext telephone numbers to hashed telephone numbers to improve security.
+For the migration, it was necessary to introduce Nullable types which should be updated with this tool :) 
---
+++ migration_software/sha256_hasher/src/main.rs
@@ -0,0 +1,66 @@
+use data_encoding::HEXUPPER;
+use ring::digest;
+
+use core::models::{Contact, User};
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+fn main() {
+    let connection = establish_connection();
+
+    update_user(&connection);
+    update_contact(&connection);
+}
+
+fn update_user(connection: &PgConnection) {
+    use core::schema::contacts::dsl::{contacts, target_tele_num};
+    use core::schema::users::dsl::{hash_tele_num, id, users};
+
+    let mut result = users.load::<User>(connection).unwrap();
+
+    for user in result.into_iter() {
+        let target = users.filter(id.eq(user.id));
+
+        diesel::update(target)
+            .set(
+                (hash_tele_num.eq(Some(HEXUPPER.encode(
+                    digest::digest(&digest::SHA256, user.tele_num.as_bytes()).as_ref(),
+                )))),
+            )
+            .execute(connection)
+            .unwrap();
+    }
+}
+
+fn update_contact(connection: &PgConnection) {
+    use core::schema::contacts::dsl::{contacts, from_id, target_hash_tele_num, target_tele_num};
+
+    let mut result = contacts.load::<Contact>(connection).unwrap();
+
+    for contact in result.into_iter() {
+        let target = contacts.filter(
+            from_id
+                .eq(contact.from_id)
+                .and(target_tele_num.eq(contact.target_tele_num.clone())),
+        );
+
+        diesel::update(target)
+            .set(
+                (target_hash_tele_num.eq(Some(HEXUPPER.encode(
+                    digest::digest(&digest::SHA256, contact.target_tele_num.as_bytes()).as_ref(),
+                )))),
+            )
+            .execute(connection)
+            .unwrap();
+    }
+}
