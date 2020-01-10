6fdb2e2af27e7c505bf87d14ed8a7dae04f29e47
Kevin Per <kevin.per@protonmail.com>
2020-01-10 11:04:02 +0100

    Update led_script

--- led_reset.sh
+++ led_reset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-. /root/gehma_server/.env && psql $DATABASE_URL -c "UPDATE users SET led = false;";
+. /home/web/gehma_server/.env && psql $DATABASE_URL -c "UPDATE users SET led = false WHERE led = true;";
