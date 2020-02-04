96b343e3c6fbdb2c1884ea29c9da628a38903138
Kevin Per <kevin.per@protonmail.com>
2020-02-04 10:10:47 +0100

    Add exporting sql view

---
+++ migrations/2020-02-04-090950_stat_monthly/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+
+CREATE VIEW stat_monthly AS (
+    SELECT tele_num, date_part('month', usage_statistics.created_at::date) as monthly, date_part('year', usage_statistics.created_at::date) as yearly, COUNT(usage_statistics.created_at::date), (dense_rank() over (ORDER BY date_part('year', usage_statistics.created_at::date) ASC, date_part('month', usage_statistics.created_at::date) ASC)) as period FROM usage_statistics  GROUP BY tele_num, monthly, yearly ORDER
+    BY yearly ASC, monthly ASC
+);
