39bce5de1b2d7a4db3555b2d0a74e561b571b504
Kevin Per <kevin.per@protonmail.com>
2020-02-07 15:04:22 +0100

    Fixing bug #48

---
+++ migrations/2020-02-07-140254_contact_view3/down.sql
@@ -0,0 +1,8 @@
+-- This file should undo anything in `up.sql`
+CREATE OR REPLACE VIEW contact_view AS (
+    SELECT contacts.from_id, c2.name, u.firebase_token FROM contacts 
+    JOIN users u ON contacts.target_hash_tele_num = u.hash_tele_num 
+    JOIN users u2 ON u2.id = contacts.from_id 
+    JOIN contacts c2 ON c2.from_id = u.id AND c2.target_hash_tele_num = u2.hash_tele_num
+    WHERE NOT EXISTS(SELECT * FROM blacklist WHERE (hash_blocker = u2.hash_tele_num and hash_blocked = contacts.target_hash_tele_num) or (hash_blocked = u2.hash_tele_num and hash_blocker = contacts.target_hash_tele_num))
+);
