a81daa8b6c211172cd2791d415d4cbbf13535608
Kevin Per <kevin.per@protonmail.com>
2020-02-23 08:40:58 +0100

    Working on images

--- img_profile/src/lib.rs
+++ img_profile/src/lib.rs
@@ -1,14 +1,68 @@
-use image::{RgbaImage};
+use image::RgbaImage;
 use rand::prelude::*;
 
-const MIN: usize = 1;
+const MIN: usize = 50;
+const RECT_HEIGHT: u32 = 50;
+const RECT_WIDTH: u32 = 50;
+
+fn get_rect(x: u32, y: u32, height: u32, width: u32) -> Vec<(u32, u32)> {
+    let mut points = Vec::with_capacity((RECT_HEIGHT * RECT_WIDTH) as usize);
+    /*
+    points.push((x, y));
+    
+    //up
+    points.extend((y..(y + RECT_HEIGHT / 2)).map(|w| (x, w)));
+    
+    //down
+    points.extend((y..(y - RECT_HEIGHT / 2)).map(|w| (x, w)));
+
+    //left
+    points.extend((x..(x - RECT_WIDTH / 2)).map(|w| (w, y)));
+
+    //right
+    points.extend((x..(x + RECT_WIDTH / 2)).map(|w| (w, y)));
+    */
+
+    for i in x..(x + RECT_WIDTH) {
+        for j in y..(y + RECT_HEIGHT) {
+            points.push((i,j));
+        }
+    }
+
+    //points.into_iter().filter(|(x, y)| (x >= &width || x < &0) || (y < &0 || y >= &height)).collect()
+    points.into_iter().filter(|(x, y)| (x < &width && x >= &0) && (y >= &0 && y < &height)).collect()
+}
+
+fn change_opacity(x: u8, P: u8) -> u8 {
+    //255 - P*(255-X)
+    255 - ((P as u32 * (255 - x) as u32)  * 100) as u8
+}
+
 
 pub fn generate(height: u32, width: u32, path: String) -> Result<(), std::io::Error> {
     let imgx = height;
     let imgy = width;
 
     //https://clrs.cc/
-    let colors = vec![[0, 31, 0, 255], [0, 116, 217, 255], [127, 219, 255, 255], [57, 204, 204, 255], [61,153,112, 255], [46,204,64, 255], [255,220,0, 255], [255,133,27, 255], [255,65,54, 255]];
+    let p: u8 = thread_rng().gen_range(67, 100);
+
+    let colors = vec![
+        [0, 31, 0, 255],
+        [0, 116, 217, 255],
+        [127, 219, 200, 255],
+        [57, 204, 204, 255],
+        [61, 153, 112, 255],
+        [46, 204, 64, 255],
+        [255, 220, 0, 255],
+        [255, 133, 27, 255],
+        [255, 65, 54, 255],
+    ]
+    .into_iter()
+    .map(|color| {
+        let [a, b, c, d] = color;
+        [change_opacity(a, p), change_opacity(b, p), change_opacity(c, p), d]
+
+    }).collect::<Vec<_>>();
 
     // Create a new ImgBuf with width: imgx and height: imgy
     let mut imgbuf: RgbaImage = image::ImageBuffer::new(imgx, imgy);
@@ -21,82 +75,24 @@ pub fn generate(height: u32, width: u32, path: String) -> Result<(), std::io::Er
         *pixel = image::Rgba([255, 255, 255, 255]);
     }
 
-    let n: usize = thread_rng().gen_range(MIN, 3);
+    let n: usize = thread_rng().gen_range(MIN, 130);
 
-    //horizontal
-    for _ in 0..n {
-        let x_0: i32 = 0;
-        let y_0: i32 = thread_rng().gen_range(0, height as i32);
-        let y_1: i32 = y_0;
-        let x_1: i32 = width as i32;
+    for i in 0..n {
+        let x = thread_rng().gen_range(0, width - 1);
+        let y = thread_rng().gen_range(0, height - 1);
 
-        println!("Punkt 1 ({}, {})", x_0, y_0);
-        println!("Punkt 2 ({}, {})", x_1, y_1);
+        let rect = get_rect(x, y, height, width);
+        let c = thread_rng().gen_range(0, colors.len());
+        let color = colors[c];
 
-        let k: f32 = (y_1 - y_0) as f32 / (x_1 - x_0) as f32; //delta y / delta x
-        let d = y_0;
+        for (x, y) in rect {
+            let mut pixel = imgbuf.get_pixel_mut(x, y);
 
-        println!("{} * x + {}", k, d);
-
-        for i in 0..width {
-            let y = d as u32;
-
-            let pixel = imgbuf.get_pixel_mut(i, y);
-            *pixel = image::Rgba([0, 0, 0, 255]);
+            *pixel = image::Rgba(color);
         }
     }
 
-    let n: usize = thread_rng().gen_range(MIN, 2);
-
-    //vertical
-    for _ in 0..n {
-        let x_0: i32 = thread_rng().gen_range(0, width as i32);
-        let y_0: i32 = 0;
-        let y_1: i32 = height as i32;
-        let x_1: i32 = x_0;
-
-        println!("Punkt 1 ({}, {})", x_0, y_0);
-        println!("Punkt 2 ({}, {})", x_1, y_1);
-
-        let k: f32 = (y_1 - y_0) as f32 / (x_1 - x_0) as f32; //delta y / delta x
-        let d = y_0;
-
-        for i in 0..height {
-            let pixel = imgbuf.get_pixel_mut(x_0 as u32, i as u32);
-            *pixel = image::Rgba([0, 0, 0, 255]);
-        }
-    }
-
-    let n: usize = thread_rng().gen_range(1, 10);
-
-    let builder = std::thread::Builder::new()
-        .name("reductor".into())
-        .stack_size(128 * 1024 * 1024); // 32MB of stack space
-
-    let handler = builder
-        .spawn(move || {
-            for  _ in 0..n {
-                let x = thread_rng().gen_range(0, width);
-                let y = thread_rng().gen_range(0, height);
-                let color = thread_rng().gen_range(0, colors.len());
-
-                fill(
-                    &mut imgbuf,
-                    colors[color],
-                    x as i32,
-                    y as i32,
-                    height,
-                    width,
-                );
-            }
-
-            imgbuf.save(path).unwrap();
-
-        })
-        .unwrap();
-
-    handler.join().unwrap();
-    
+    imgbuf.save(path).unwrap();
 
     Ok(())
 }
