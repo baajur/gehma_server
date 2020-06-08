0f09da2cab797112b8d540a2249fa7ba81a13566
Kevin Per <kevin.per@protonmail.com>
2020-06-08 07:55:58 +0200

    Changing debug prints

--- gehma_server/src/controllers/number_registration.rs
+++ gehma_server/src/controllers/number_registration.rs
@@ -3,7 +3,7 @@ use core::errors::{InternalServerError, InvalidUserInput, ServiceError};
 use core::models::dto::*;
 use core::models::PhoneNumber;
 
-use log::{debug, error, info};
+use log::{debug, error, info, trace};
 
 use crate::queries::*;
 use crate::services::number_registration::NumberRegistrationService;
@@ -14,11 +14,13 @@ pub(crate) fn request(
     body: RequestCodeDto,
     number_registration_service: web::Data<NumberRegistrationService>,
 ) -> Result<(), ServiceError> {
-    info!("controllers/auth/request_code");
+    trace!("controllers/auth/request_code");
 
     let parsed = PhoneNumber::my_from(&body.tele_num, &body.country_code)?;
 
     number_registration_service.request_code(&parsed)?;
+
+    info!("Code was requested");
 
     Ok(())
 }
@@ -28,22 +30,31 @@ pub(crate) fn check_code(
     user_dao: web::Data<Box<dyn PersistentUserDao>>,
     number_registration_service: web::Data<NumberRegistrationService>,
 ) -> Result<UserDto, ServiceError> {
-    info!("controllers/auth/check_code");
+    trace!("controllers/auth/check_code");
 
     let parsed = PhoneNumber::my_from(&body.tele_num, &body.country_code)?;
 
     let res = number_registration_service.check_code(&parsed, &body.code)?;
 
+
     // Create a new user when the code was correct
     if res {
+        info!("Code is correct");
+
         // Check if a user already exists
         match user_dao.get_ref().get_by_tele_num(&parsed) {
             Ok(user) => {
-                let path = user_dao.get_profile_picture(&user)?;
+                let path = user_dao.get_profile_picture(&user).map_err(|err| {
+                    error!("Profile picture {:?}", err); 
+                    err
+                })?;
                 return Ok(user.into(path))
             },
             Err(ServiceError::ResourceDoesNotExist) => debug!("User does not exist. Inserting"),
-            Err(e) => return Err(e),
+            Err(e) => {
+                error!("{:?}", e);
+                return Err(e)
+            },
         }
 
         // If not then create one
