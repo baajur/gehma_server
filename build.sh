923f3dc7e4d69309bdd545698b1390eccab5264f
Kevin Per <kevin.per@protonmail.com>
2020-06-09 14:10:02 +0200

    Updating build scripts

--- build.sh
+++
@@ -1,1 +0,0 @@
-sudo docker build -t kper/gehma:$1 . && cd .. && sudo docker save kper/gehma:$1 -o gehma_$1 && sudo chown kper:kper gehma_$1 && bzip2 gehma_$1
---
+++ gehma_server/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [$1 = ""]
+then
+	echo "Please add a version like v0.x.0"
+	exit -1
+fi
+
+echo "Building kper/gehma:$1" \
+&& docker build -t kper/gehma:$1 .  \
+&& echo "Saving kper/gehma:$1 to ~/images/gehma_$1" \
+&& docker save kper/gehma:$1 -o ~/images/gehma_$1  \
+&& echo "Bzip2 ~/images/gehma_$1" \
+&& bzip2 ~/images/gehma_$1 \
+echo "Finished"
